16469f7f4d698ab226df813fd26a3e6d7d7c7c27
marqzxq <77423720+marqzxq@users.noreply.github.com>
2021-01-23 01:13:15 -0300

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,33 @@
+const menuadmin = (prefix, pushname) => {
+ return `esse é o menu dos admins
+ _o bot precisa ser adm para executar esses comandos_
+ 
+ ◪ *COMANDO DOS ADMINS*
+ │
+ ├─ ❏ ${prefix}opengc
+ ├─ ❏ ${prefix}closegc
+ ├─ ❏ ${prefix}promote
+ ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}tagall2
+ ├─ ❏ ${prefix}tagall3
+ ├─ ❏ ${prefix}tagall4
+ ├─ ❏ ${prefix}tagall5
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}kick
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}welcome
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}leveling
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
---
+++ src/nsfwmenu.js
@@ -0,0 +1,22 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `resumindo aqui é o menu NSFW
+    ◪ *NSFW*
+  │
+  ├─ ❏ ${prefix}nsfwbobs
+  ├─ ❏ ${prefix}randomhentaio
+  ├─ ❏ ${prefix}nsfwtrap
+  ├─ ❏ ${prefix}nsfwass
+  ├─ ❏ ${prefix}nsfwbelly
+  ├─ ❏ ${prefix}nsfwsidebobs
+  ├─ ❏ ${prefix}nsfwahegao
+  ├─ ❏ ${prefix}nsfwthighs
+  ├─ ❏ ${prefix}nsfwarmpits
+  └─ ❏ ${prefix}nsfwfeets
+
+  _para usar esses comandos ative o menu NSFW_\n _Digite_\n ${prefix}*nsfw 1*`
+
+
+
+}
+
+exports.nsfwmenu = nsfwmenu
