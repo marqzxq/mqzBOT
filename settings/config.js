d74db9e6b279032b79b4f862b4051d04575ca6b5
marqzxq <77423720+marqzxq@users.noreply.github.com>
2021-01-23 01:10:55 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'mqzBOT',
+        ownerName: 'marquin7',
+        youtube: 'https://youtube.com/c/MarquinXff',
+        instagram: 'https://instagram.com/marquin.xz?igshid=9j5myqreillp',
+}
